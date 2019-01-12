a7dbd77b6e0175b668f59f50c3189b76c7bf68ba
Orjon <github@orjon.com>
2019-01-12 16:23:29 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,28 @@
 console.log("Up and running!");
 
-var cardOne   = "queen";
-var cardTwo   = "queen";
-var cardThree = "king";
-var cardFour  = "king";
+// var cardOne   = "queen";
+// var cardTwo   = "queen";
+// var cardThree = "king";
+// var cardFour  = "king";
 
-console.log("User flipped " + cardOne + " and " + cardThree);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = []; //Initially set to empty.
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
