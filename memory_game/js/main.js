246ae54f46ce539a69ba43c1b521c5fc3c33c1f4
Orjon <github@orjon.com>
2019-01-16 01:09:33 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,46 @@
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"	
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"	
+}
+];
+
+
 var cardsInPlay = []; //Initially set to empty.
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
-		console.log("Sorry, try again");
+		alert("Sorry, try again");
 		}
 }
 
 function flipCard(cardId) {
   
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("Suit " + cards[cardId].suit);
+	console.log("Card Image  " + cards[cardId].cardImage);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 
 	if (cardsInPlay.length === 2) {
@@ -25,5 +50,6 @@ function flipCard(cardId) {
 
 flipCard(0);
 flipCard(2);
+
 
 
